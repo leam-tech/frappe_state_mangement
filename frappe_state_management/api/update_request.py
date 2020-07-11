58d8a347ed6657ee8aaeb6e255c9b575a95d1206
Assem Marwan <assem905@gmail.com>
2020-07-11 11:20:43 +0400

    feat: Added API to revert

--- frappe_state_management/api/update_request.py
+++ frappe_state_management/api/update_request.py
@@ -2,6 +2,8 @@ import frappe
 from frappe.utils import now_datetime
 from frappe_state_management.frappe_state_management.doctype.update_request.update_request import UpdateRequest
 
+dt = 'Update Request'
+
 
 @frappe.whitelist()
 def approve(update_request: str) -> dict:
@@ -12,7 +14,7 @@ def approve(update_request: str) -> dict:
   """
   user = frappe.session.user
 
-  update_request_doc: UpdateRequest = frappe.get_doc('Update Request', update_request)
+  update_request_doc: UpdateRequest = frappe.get_doc(dt, update_request)
   update_request_doc.status = 'Approved'
   update_request_doc.approved_by = user
   update_request_doc.approved_on = now_datetime()
@@ -29,9 +31,21 @@ def reject(update_request: str) -> dict:
   """
   user = frappe.session.user
 
-  update_request_doc: UpdateRequest = frappe.get_doc('Update Request', update_request)
+  update_request_doc: UpdateRequest = frappe.get_doc(dt, update_request)
   update_request_doc.status = 'Rejected'
   update_request_doc.rejected_by = user
   update_request_doc.rejected_on = now_datetime()
   update_request_doc.save()
   return update_request_doc.as_dict()
+
+
+@frappe.whitelist()
+def revert(update_request: str) -> dict:
+  """
+  Revert an update request.
+  Given the Update Request is Successful and that it's the latest for the Doctype/Docname combination
+  :param update_request: The name of the update request
+  :return:
+  """
+  update_request_doc: UpdateRequest = frappe.get_doc(dt, update_request)
+  return update_request_doc.revert()
